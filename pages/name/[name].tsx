526cd8828e31e291834c688250429fa4e3704852
Moisés Barillas <30334174+moises717@users.noreply.github.com>
2022-08-30 19:31:15 -0600

    Incremental static generation

--- pages/name/[name].tsx
+++ pages/name/[name].tsx
@@ -82,7 +82,7 @@ export const getStaticPaths: GetStaticPaths = async (ctx) => {
 
     return {
         paths: pokeNames.map(name => ({ params: { name } })),
-        fallback: false,
+        fallback: 'blocking',
     }
 }
 
@@ -90,10 +90,20 @@ export const getStaticPaths: GetStaticPaths = async (ctx) => {
 export const getStaticProps: GetStaticProps = async ({ params }) => {
 
     const { name } = params as { name: string };
+    const pokemon = await getPokemonInfo(name);
+
+    if (!pokemon) {
+        return {
+            redirect: {
+                destination: '/',
+                permanent: false
+            }
+        }
+    }
 
     return {
         props: {
-            pokemon: await getPokemonInfo(name),
+            pokemon
         },
 
     }
--- utils/getPokemonInfo.ts
+++ utils/getPokemonInfo.ts
@@ -2,11 +2,15 @@ import {pokeApi} from '../api';
 import {Pokemon} from '../interfaces';
 
 export const getPokemonInfo = async (param: string) => {
-	const {data} = await pokeApi.get<Pokemon>(`/pokemon/${param}`);
+	try {
+		const {data} = await pokeApi.get<Pokemon>(`/pokemon/${param}`);
 
-	return {
-		name: data.name,
-		sprites: data.sprites,
-		id: data.id,
-	};
+		return {
+			name: data.name,
+			sprites: data.sprites,
+			id: data.id,
+		};
+	} catch (error) {
+		return null;
+	}
 };
